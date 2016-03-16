b8df70d61a685d333f481431414bd30ded93f673
Edward Granger <edward.b.granger@lowes.com>
2016-03-15 22:59:51 -0400

    moving folders around

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
 	inlineCss = require('gulp-inline-css');
 
 gulp.task('default', function() {
-    return gulp.src('./*.html')
+    return gulp.src('./html/*.html')
         .pipe(inlineCss())
         .pipe(gulp.dest('./'));
 });
