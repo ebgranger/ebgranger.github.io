7145da0fea860a4c1fad9ef5e0f921d40e40cda7
Edward Granger <edward.b.granger@lowes.com>
2016-03-15 23:15:45 -0400

    adding html minifier

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
-	inlineCss = require('gulp-inline-css');
+	inlineCss = require('gulp-inline-css'),
+	htmlmin = require('gulp-htmlmin');
 
 gulp.task('default', function() {
     return gulp.src('./html/*.html')
         .pipe(inlineCss())
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('./'));
 });
